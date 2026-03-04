477666deb9cd5f7f0875a76918e88e42698e8d5f
AKaiser00 <andrewkaiser05@gmail.com>
2026-03-04 15:49:35 -0500

    created another exception to deal with frozen accounts

---
+++ server/src/exceptions/account_frozen_exception.py
@@ -0,0 +1,19 @@
+class AccountFrozen(Exception):
+    """
+    Exception raised when an account has been frozen but a transaction was attempted.
+    """
+
+    def __init__(self, account_number: int):
+        """
+        Initialize the AccountFrozenException.
+
+        Args:
+            amount (float): The account number of the account frozen.
+        """
+        self.account_number = account_number
+
+        super().__init__(f"Cannot perform this transaction on the frozen account with account number of {self.account_number}.")
+            
+
+
+   
