04af19eaa97e68ec6b3bc7819971982f084aad55
AKaiser00 <andrewkaiser05@gmail.com>
2026-03-04 10:45:19 -0500

    created skeleton code for two exceptions

---
+++ server/src/exceptions/withdraw_maxed_exception.py
@@ -0,0 +1,15 @@
+class WithdrawMaxedException(Exception):
+    """
+    Exception raised when the withdrawal limit has been reached for savings accounts.
+    """
+
+    def __init__(self, max_withdraw_limit: float, curr_withdraw_limit: float, withdraw_amount: float):
+        """
+        Initialize the WithdrawMaxedException.
+
+        Args:
+            max_withdraw_limit (float): The maximum withdrawal limit.
+            curr_withdraw_limit (float): The current withdrawal limit.
+            withdraw_amount (float): The amount attempted to be withdrawn.
+        """
+        pass
