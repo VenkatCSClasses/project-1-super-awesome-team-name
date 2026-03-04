ecec645a05bfecb9ad1ceff1b08cdda4367919de
drweb <dwebb@ithaca.edu>
2026-03-04 11:14:25 -0500

    added customer tests for existing skeleton code

--- server/tests/customer_test.py
+++ server/tests/customer_test.py
@@ -1,5 +1,23 @@
 import pytest
+from .customer import Customer
 
 class TestCustomer:
-    def test_hello(self):
-        assert 1 == 1
+    def test_get_name(self):
+        test = Customer("john", 5, "password")
+        assert test.name == "john"
+    
+    def test_get_id(self):
+        test = Customer("john", 5, "password")
+        assert test.id == 5
+
+    def test_get_password(self):
+        test = Customer("john", 5, "password")
+        assert test.password == "password"
+
+    def test_get_acc_type(self):
+        test = Customer("john", 5, "password")
+        assert test.accType == 0
+
+    def test_get_acc_details(self):
+        test = Customer("john", 5, "password")
+        assert test.get_acc_details() == "Name: john\nID: 5\naccType: Customer"
