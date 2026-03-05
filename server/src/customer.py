5b3a84615162c309ec985308775ac9b41c7ba963
drweb <dwebb@ithaca.edu>
2026-03-05 11:25:43 -0500

    changed customer to be a super class; removed abc functionality and added setters + getters

--- server/src/customer.py
+++ server/src/customer.py
@@ -1,6 +1,4 @@
-from server.src.user import User
-
-class Customer(User):
+class Customer():
     def __init__(self, name, id, passwd):
         self.name = name
         self.id = id
@@ -8,33 +6,42 @@ class Customer(User):
         self.accType = 0
         self.accounts = []
 
-    @property
-    def name(self):
+    def get_name(self):
         return self.name
     
-    @property
-    def id(self):
-        return self.id
+    def set_name(self, name:str):
+        self.name = name
 
-    @property  
-    def password(self):
+
+    def get_id(self):
+        return self.id
+    
+    def set_id(self, id:int):
+        self.int = self.int
+
+
+    def get_passwd(self):
         return self.passwd
     
-    @property
-    def acc_type(self):
+    def set_passwd(self, pss:str):
+        self.passwd = pss
+
+
+    def get_accType(self):
         return self.accType
     
+    def set_accType(self, type:int):
+        self.accType = type
+
+
+    def get_accounts(self):
+        return self.accounts
+        
     """
     returns name, id, and customer in visual string
     """
     def get_acc_details(self):
         return "Name:" , self.name , "\nID:" , self.id , "\naccType: Customer"
-    
-    """
-    returns the customer's accounts
-    """
-    def get_accounts(self):
-        return self.accounts
 
     """
     returns a formatted string containing all transactions in an accounts history
