0dc379e278ff3b309e9a14bf9b4e459f2a61d0ac
untitledham <abemanfra@gmail.com>
2026-03-05 16:50:32 -0500

    Removed models file

--- server/models.py
+++
@@ -1,8 +0,0 @@
-from sqlmodel import SQLModel, Field
-from typing import Optional
-
-class User(SQLModel, table=True):
-    id: Optional[int] = Field(default=None, primary_key=True)
-    username: str
-    hashed_password: str
-    permission: Optional[int] = Field(default=0, nullable=False, sa_column_kwargs={"server_default": "0"})
