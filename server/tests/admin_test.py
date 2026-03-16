6b27c2ba02f0bee1eb85a3d4ee392ad734b578c1
drweb <dwebb@ithaca.edu>
2026-03-15 21:58:30 -0400

    added comments to teller admin and customer tests to declare type; added code for unfinished teller test

--- server/tests/admin_test.py
+++ server/tests/admin_test.py
@@ -10,32 +10,37 @@ from exceptions.account_frozen_exception import AccountFrozenException
 class TestAdmin:
     
     def test_get_name(self):
+        """integration test: tests proper input for constructor"""
         bank = Bank()
         test = Admin("john", 5, "password", bank)
         assert test.get_name() == "john"
     
     def test_get_id(self):
+        """integration test: tests proper input for constructor"""
         bank = Bank()
         test = Admin("john", 5, "password", bank)
         assert test.get_id() == 5
 
     def test_get_password(self):
+        """integration test: tests proper input for constructor"""
         bank = Bank()
         test = Admin("john", 5, "password", bank)
         assert test.get_passwd() == "password"
 
     def test_get_permissions(self):
+        """integration test: tests proper input for constructor"""
         bank = Bank()
         test = Admin("john", 5, "password", bank)
         assert test.get_permissions() == 2
 
     def test_get_user_acc_details(self):
+        """integration test: tests proper input for constructor"""
         bank = Bank()
         test = Admin("john", 5, "password", bank)
         assert test.get_user_acc_details() == ('Name:', 'john' , '\nID:', 5 , '\naccType: Admin')
 
     def test_check_sus_activity(self):
-        #TODO: not sure if this is how we're checking it
+        """integration test: tests varying levels of withdrawals to determine effective flagging"""
         bank = Bank()
         test = Admin("john", 5, "password", bank)
         acc = CheckingAccount(4, bank, 50000.00)
@@ -46,6 +51,7 @@ class TestAdmin:
         assert test.check_sus_activity(acc2)
 
     def test_toggle_frozen(self):
+        """integration test: ensures freeze effect is applied at the correct times"""
         bank = Bank()
         test = Admin("john", 5, "password", bank)
         acc = CheckingAccount(1, bank, 100)
--- server/tests/customer_test.py
+++ server/tests/customer_test.py
@@ -8,31 +8,38 @@ from bank import Bank
 class TestCustomer:
     
     def test_get_name(self):
+        """integration test: tests proper input for constructor"""
         bank = Bank()
         test = Customer("john", 5, "password", bank)
         assert test.get_name() == "john"
     
     def test_get_id(self):
+        """integration test: tests proper input for constructor"""
         bank = Bank()
         test = Customer("john", 5, "password", bank)
         assert test.get_id() == 5
 
     def test_get_password(self):
+        """integration test: tests proper input for constructor"""
         bank = Bank()
         test = Customer("john", 5, "password", bank)
         assert test.get_passwd() == "password"
 
     def test_get_acc_type(self):
+        """integration test: tests proper input for constructor"""
         bank = Bank()
         test = Customer("john", 5, "password", bank)
         assert test.get_permissions() == 0
 
     def test_get_user_acc_details(self):
+        """integration test: tests proper input for constructor"""
         bank = Bank()
         test = Customer("john", 5, "password", bank)
         assert test.get_user_acc_details() == ('Name:', 'john' , '\nID:', 5 , '\naccType: Customer')
 
     def test_get_total_transact_hist(self):
+        """integration test: ensures transaction history of a given bank is accurately tracked across multiple transactions
+        at different points"""
         bank = Bank()
         test = Customer("john", 5, "password", bank)
 
